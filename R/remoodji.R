20322df6bd9957b29f0a200b2e96a65b0aed34fc
Rachel Wong <rachelwong.main@gmail.com>
2021-03-05 15:29:52 -0800

    adding to repo now

--- R/remoodji.R
+++ R/remoodji.R
@@ -5,12 +5,12 @@
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a factor
-#' @param b factor
+#' @param text factor
+#' @param sentiment factor
 #'
 #' @return factor
 #' @export
-#' @examples
+#' #examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 sentiment_df <- function(text, sentiment) {
 }
@@ -20,36 +20,42 @@ sentiment_df <- function(text, sentiment) {
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a factor
-#' @param b factor
+#' @param text factor
+#' @param sentiment factor
 #'
 #' @return factor
 #' @export
-#' @examples
+#' #examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+emoji <- function(text, sentiment) {
+}
 
 #' Sentiment Plot Function
 #'
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a factor
-#' @param b factor
+#' @param text factor
+#' @param sentiment factor
 #'
 #' @return factor
 #' @export
-#' @examples
+#' #examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+sentiment_plot <- function(text, sentiment) {
+}
 
 #' Counter Function
 #'
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a factor
-#' @param b factor
+#' @param text factor
+#' @param sentiment factor
 #'
 #' @return factor
 #' @export
-#' @examples
+#' #examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+counter <- function(text, sentiment) {
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(remoodji)
+
+test_check("remoodji")
